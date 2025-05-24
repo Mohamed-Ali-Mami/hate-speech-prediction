b778eee0fc0a29df23938baae33de4817380fbb9
daly <dalymami741@outlook.com>
2025-05-23 21:50:57 -0400

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="Daly",
+    author_email="dalymami741@outlook.com",
+    packages=find_packages(),
+    install_requires=[]
+)
